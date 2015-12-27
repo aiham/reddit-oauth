c036da550a765231a0a977b23f437ec834c79fa2
Aiham Hammami <aiham.hammami@servian.com.au>
2015-12-27 11:59:50 +1100

    Move RedditRequest class into its own module. Add test file for it

---
+++ test/request.js
@@ -0,0 +1,12 @@
+var assert = require('assert');
+var RedditRequest = require('../lib/request');
+
+describe('RedditRequest', function () {
+  describe('new RedditRequest()', function () {
+    it('should initialise the object successfully', function () {
+    });
+  });
+
+  describe('send()', function () {
+  });
+});
